f9c10ed92ceceaaa5f1e257a042aee258ae012af
Methuz Kaewsaikhao <methuz.k@gmail.com>
2018-01-10 13:54:08 +0700

    Add waiting, joining, private message function

--- main.js
+++ main.js
@@ -2,22 +2,64 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const path = require('path')
+const debug = require('debug')('main')
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-io.on('connection', function (socket) {
-  console.log('a user connected')
-  socket.on('disconnect', function () {
-    console.log('user disconnected')
+const waitingList = []
+const rooms = []
+
+io.on('connection', (socket) => {
+  socket.on('waiting', () => {
+    waitingList.push(socket.id)
+    debug(`User ${socket.id} is waiting`)
+
+    debug('waitingList.length = ', JSON.stringify(waitingList.length, null, 4))
+    if (waitingList.length > 1) {
+      debug('connecting users')
+      const user1 = waitingList.pop()
+      const user2 = waitingList.pop()
+
+      const sortedUsers = [user1, user2].sort()
+      const roomId = `${sortedUsers[0]}_${sortedUsers[1]}`
+
+      io.sockets.connected[user1].emit('join room', roomId)
+      io.sockets.connected[user2].emit('join room', roomId)
+
+      rooms.push({
+        users: [
+          user1,
+          user2
+        ],
+        room_id: roomId
+      })
+    }
   })
-  socket.on('chat message', function (msg) {
+
+  debug(`User ${socket.id} connected`)
+
+  socket.on('disconnect', () => {
+    debug('user disconnected')
+  })
+
+  socket.on('join room', (roomId) => {
+    debug('join room', roomId)
+    socket.join(roomId)
+  })
+
+  socket.on('private message', (data) => {
+    debug('data = ', JSON.stringify(data, null, 4))
+    socket.broadcast.to(data.room_id).emit('private message', data.message)
+  })
+
+  socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', msg)
-    console.log('message: ' + msg)
+    debug('message: ' + msg)
   })
 })
 
-http.listen(3000, function () {
-  console.log('listening on *:3000')
+http.listen(3000, () => {
+  debug('listening on port 3000')
 })
